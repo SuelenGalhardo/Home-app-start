7a8b771150311e480732b93b755f15e429e3b835
SuelenGalhardo <suelenranucci@gmail.com>
2024-06-05 11:09:17 +0200

    added routes and comp Details

--- src/app/housingLocation/housingLocation.component.ts
+++ src/app/housingLocation/housingLocation.component.ts
@@ -1,18 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HousingLocation} from '../housingLocation'
+import { HousingLocation} from '../housingLocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housingLocation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
   <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
 
     <h2 class="listing-heading">{{housingLocation.name}}</h2>
     <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+  <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+
   </section>
+
   `,
   styleUrls: ['./housingLocation.component.css']
 })
@@ -21,3 +25,4 @@ export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
 
 }
+//    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
