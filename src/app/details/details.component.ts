256af1f74ba70754fc7340ef0b3ad4be3f1de5a0
SuelenGalhardo <suelenranucci@gmail.com>
2024-06-10 14:17:45 +0200

    added form

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housingLocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
 
 
@@ -28,6 +29,18 @@ import { HousingLocation } from '../housingLocation';
     <section class="listing-apply">
       <h2 class="section-heading">Apply now to live</h2>
       <button class="primary" type="button">Apply now </button>
+
+    <form [formGroup]="applyForm" (submit)= "submitApplication()" >
+    <label for="first-name">  First Name </label>
+    <input type="text" id="first-name" formControlName="">
+
+    <label for="last-name">  Last Name </label>
+    <input type="text" id="last-name" formControlName="lastName">
+
+    <label for="email">  Email </label>
+    <input type="text" id="email" formControlName="email">
+    <button class="primary" type="submit">Apply now </button>
+    </form>
     </section>
    </article>
 
@@ -38,10 +51,24 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject (HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({ 
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+   
+  })
 
 
   constructor() {
      const housingLocationId = Number(this.route.snapshot.params['id']);
      this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName?? '',
+      this.applyForm.value.email?? '',
+    
+    )
   }
 }
